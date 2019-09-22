4c073abc009ff47c53f205bd42cdf7d33943d42f
Alex Simisi <smsxander@gmail.com>
2019-09-22 22:00:18 +0200

    Changement de page avec React Router V4

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="box">
+        <h2>Titre</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde deleniti
+          voluptatum reiciendis doloremque? Aliquam, dolor esse? Iste, possimus
+          nulla. Sequi nostrum quia, atque nihil iusto placeat dolor dolores
+          tenetur earum.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Connexion.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import "../App.css";
+
+class Connexion extends Component {
+  state = {
+    pseudo: "",
+    goToChat: false
+  };
+
+  handleChange = event => {
+    const pseudo = event.target.value;
+    this.setState({ pseudo });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({ goToChat: true });
+  };
+
+  render() {
+    let { pseudo, goToChat } = this.state;
+
+    if (goToChat) {
+      return <Redirect to={`/pseudo/${pseudo}`}></Redirect>;
+    }
+
+    return (
+      <div className="connexionBox">
+        <form className="connexion" onSubmit={this.handleSubmit}>
+          <input
+            value={pseudo}
+            onChange={this.handleChange}
+            type="text"
+            placeholder="Pseudo"
+            required
+          />
+          <button type="submit">Go</button>
+        </form>
+      </div>
+    );
+  }
+}
+export default Connexion;
